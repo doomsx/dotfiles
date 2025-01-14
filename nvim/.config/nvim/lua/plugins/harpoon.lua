6cca5bcab95231ca4347c7d11fa4412f6723168a
Doms <dlmagat08@proton.me>
2025-01-14 22:45:57 +0800

    update

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,50 +2,43 @@ return {
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    lazy = false,
-    keys = {
-      {
-        "<leader>a",
-        function()
-          require("harpoon.mark").add_file()
-        end,
-        desc = "Add file to Harpoon",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
       },
-      {
-        "<C-e>",
-        function()
-          require("harpoon.ui").toggle_quick_menu()
-        end,
-        desc = "Toggle Harpoon Menu",
-      },
-      {
-        "<F1>",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = "Navigate to Harpoon File 1",
-      },
-      {
-        "<F2>",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = "Navigate to Harpoon File 2",
-      },
-      {
-        "<F3>",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = "Navigate to Harpoon File 3",
-      },
-      {
-        "<F4>",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = "Navigate to Harpoon File 4",
+      settings = {
+        save_on_toggle = true,
       },
     },
+    keys = function()
+      local keys = {
+        {
+          "<leader>a",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<C-e>",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          "<leader>" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
   },
 }
