5bb71556ed3ae2a6b25d8520b6356d9bc763a794
Doms <dlmagat08@proton.me>
2025-01-29 17:56:32 +0800

    add some opts

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,7 +6,11 @@ vim.opt.scrolloff = 8
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
 vim.opt.expandtab = true
 
 vim.opt.termguicolors = true
+
 vim.opt.smartindent = true
+
+vim.opt.swapfile = false
