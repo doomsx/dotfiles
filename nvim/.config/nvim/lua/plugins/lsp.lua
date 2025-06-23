f494e693a14e9507388d93ba0b0e50c1d3e951fe
Dominique <dlmagat08@gmail.com>
2025-06-23 10:30:41 +0800

    added gopls and jdtls

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -153,7 +153,8 @@ return {
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
 			-- clangd = {},
-			-- gopls = {},
+			gopls = {},
+			jdtls = {},
 			-- pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
