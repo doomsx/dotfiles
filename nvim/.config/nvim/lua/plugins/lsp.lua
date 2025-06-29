70fe7b63190157660fb7476e54a34e7414c68bfd
Dominique <dlmagat08@gmail.com>
2025-06-29 20:56:00 +0800

    lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -154,7 +154,6 @@ return {
 		local servers = {
 			-- clangd = {},
 			gopls = {},
-			jdtls = {},
 			-- pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
