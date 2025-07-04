905c29845f3ccbfaa3324183a2d9878a5e2f5137
Dominique <dlmagat08@gmail.com>
2025-07-04 19:03:49 +0800

    indent

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
 		bigfile = { enabled = true },
 		dashboard = { enabled = false },
 		explorer = { enabled = true },
-		indent = { enabled = false },
+		indent = { enabled = true },
 		input = { enabled = true },
 		notifier = {
 			enabled = true,
